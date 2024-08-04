ed726b83222c6c7ef41a9ce832bb186cb3c5d11c
Arun Khatri <akcs4444@gmail.com>
2024-08-04 15:17:06 +0545

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Banner from './Banner'
-import QueAns from './QueAns'
-import Youhavewon from './Youhavewon'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Banner from "./Banner";
+import QueAns from "./QueAns";
+import Youhavewon from "./Youhavewon";
 
 function App() {
-  const [questions, setQuestions] = useState(null)
-  const [rightAnswered, setRightAnswered] = useState(0)
-  const [timer, setTimer] = useState(60)
-  const [gameStarted, setGameStarted] = useState(false)
-  const [gameOver, setGameOver] = useState(false)
-  const [showStartButton, setShowStartButton] = useState(true)
+  const [questions, setQuestions] = useState(null);
+  const [rightAnswered, setRightAnswered] = useState(0);
+  const [timer, setTimer] = useState(60);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
+  const [showStartButton, setShowStartButton] = useState(true);
 
-  const letsPlay=new Audio('audios/letsplay.mp3');
-
+  const letsPlay = new Audio("audios/letsplay.mp3");
 
   const handleClick = () => {
     setGameStarted(true);
     letsPlay.play();
-  }
+  };
 
   useEffect(() => {
     if (!gameStarted) {
@@ -26,10 +25,10 @@ function App() {
     }
 
     const interval = setInterval(() => {
-      setTimer(prev => {
+      setTimer((prev) => {
         if (prev <= 1) {
           clearInterval(interval);
-          setGameOver(true)
+          setGameOver(true);
         }
 
         if (gameOver) {
@@ -38,47 +37,67 @@ function App() {
 
         return prev - 1;
       });
-    }, 1000)
+    }, 1000);
 
-    return () => { clearInterval(interval) }
-  }, [gameStarted, gameOver])
+    return () => {
+      clearInterval(interval);
+    };
+  }, [gameStarted, gameOver]);
 
-  const url = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy'
+  const url =
+    "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy";
 
   const getQuestions = async () => {
-    const data = await fetch(url)
+    const data = await fetch(url);
     const res = await data.json();
-    setQuestions(res.results)
-    console.log(res.results.correct_answer)
-  }
+    setQuestions(res.results);
+    console.log(res.results.correct_answer);
+  };
 
   useEffect(() => {
     getQuestions();
-  }, [])
+  }, []);
 
-  useEffect(()=>{
-  console.log(rightAnswered)
-
-  },[rightAnswered])
+  useEffect(() => {
+    console.log(rightAnswered);
+  }, [rightAnswered]);
   return (
     <>
       <Banner rightAnswered={rightAnswered} timer={timer} />
-      {!gameOver ?
-        <QueAns setShowStartButton={setShowStartButton} setTimer={setTimer} gameStarted={gameStarted} setGameStarted={setGameStarted} gameOver={gameOver} setGameOver={setGameOver} rightAnswered={rightAnswered} setRightAnswered={setRightAnswered} questions={questions} />
-        :
-        <Youhavewon rightAnswered={rightAnswered} />}
+      {!gameOver ? (
+        <QueAns
+          setShowStartButton={setShowStartButton}
+          setTimer={setTimer}
+          gameStarted={gameStarted}
+          setGameStarted={setGameStarted}
+          gameOver={gameOver}
+          setGameOver={setGameOver}
+          rightAnswered={rightAnswered}
+          setRightAnswered={setRightAnswered}
+          questions={questions}
+        />
+      ) : (
+        <Youhavewon rightAnswered={rightAnswered} />
+      )}
 
-      {!gameStarted && showStartButton && <div className='flex justify-center my-9'><button onClick={handleClick}>
-      <img  className='playbtn' width="100" height="100" src="https://img.icons8.com/flat-round/64/play--v1.png" alt="play--v1"/>
-
-      </button>
-      </div>}
-        <p className='text-center text-white text-lg'>Copyright &copy; Errun Lee, 2023 Inc</p>
+      {!gameStarted && showStartButton && (
+        <div className="flex justify-center my-9">
+          <button onClick={handleClick}>
+            <img
+              className="playbtn"
+              width="100"
+              height="100"
+              src="https://img.icons8.com/flat-round/64/play--v1.png"
+              alt="play--v1"
+            />
+          </button>
+        </div>
+      )}
+      <p className="text-center text-white text-lg">
+        Copyright &copy; Errun Lee, 2023 Inc
+      </p>
     </>
-  )
+  );
 }
 
-export default App
-
-
-
+export default App;
